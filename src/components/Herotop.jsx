25089fc79df8a826f7ba2dfde0303d3626682129
shubham200223feb <shubham2101861@gmail.com>
2026-02-24 22:48:08 +0530

    till types

--- src/components/Herotop.jsx
+++ src/components/Herotop.jsx
@@ -7,7 +7,7 @@ const Herotop = () => {
         <div className=' text-[10vw] uppercase leading-[8vw]'>
             MAKHANA 
         </div>
-        <div className='text-[10vw] uppercase leading-[8vw] flex items-center justify-center'>THAT FUELS<div><video autoPlay loop muted className='h-[20vw] object-center rounded-full' src="../../public/headeing video.mp4"/></div> YOUR DAY</div>
+        <div className='text-[10vw] uppercase leading-[8vw] flex items-center justify-center'>THAT FUELS<div><video autoPlay loop muted className='h-[20vw] object-center rounded-full' src="/headeing video.mp4"/></div> YOUR DAY</div>
             
             <div className='text-[10vw] uppercase leading-[8vw]'>YOUR WAY</div>
             </div>
